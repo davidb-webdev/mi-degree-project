11b630baa8a42d1a22b952f423785f9d21847a87
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-12-13 17:22:45 +0100

    set up i18n

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import fireInspectTheme from "./Theme";
 import { NotesDrawerProvider } from "./utils/useNotesDrawer";
+import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- client/src/views/start/IntroView.tsx
+++ client/src/views/start/IntroView.tsx
@@ -6,10 +6,12 @@ import {
   Typography,
   Link
 } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import { Link as RouterLink, useNavigate } from "react-router";
 
 const IntroView = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation("translation", { keyPrefix: "intro" });
 
   return (
     <>
@@ -17,7 +19,7 @@ const IntroView = () => {
         <Skeleton variant="rectangular" height={400} />
 
         <Stack spacing={3} sx={{ my: 3 }}>
-          <Typography variant="body1">Lorem</Typography>
+          <Typography variant="body1">{t("key")}</Typography>
           <Typography variant="body1">Ipsum</Typography>
           <Typography variant="body1">Dolor</Typography>
           <Typography variant="body1">Sit</Typography>
@@ -31,7 +33,7 @@ const IntroView = () => {
         >
           Register now
         </Button>
-        <Typography variant="body1" sx={{mb: 3}}>
+        <Typography variant="body1" sx={{ mb: 3 }}>
           or{" "}
           <Link component={RouterLink} to="/start/signin">
             Sign in
