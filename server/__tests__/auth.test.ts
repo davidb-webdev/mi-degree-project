05bb5c23a581070c8dd9d2adc491a5a6f5c5e590
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-12-23 01:59:47 +0100

    mock db calls in auth tests (wip)

--- server/__tests__/auth.test.ts
+++ server/__tests__/auth.test.ts
@@ -2,13 +2,51 @@ import request from "supertest";
 import app from "../app";
 import DatabaseConnection from "../database/DatabaseConnection";
 
-describe("Server Endpoints", () => {
-  test("should return a 200 status code", async () => {
-    const res = await request(app).get("/testdb");
-    expect(res.statusCode).toEqual(200);
-  });
-});
+jest.mock("mongodb");
 
-afterAll(async () => {
-  await DatabaseConnection.getInstance().disconnect();
+describe("Auth endpoints", () => {
+  let mockGetCollection: jest.Mock;
+  let mockConnect: jest.Mock;
+
+  beforeEach(() => {
+    mockGetCollection = jest.fn();
+    mockConnect = jest.fn();
+
+    jest
+      .spyOn(DatabaseConnection.prototype, "connect")
+      .mockImplementation(mockConnect);
+    jest
+      .spyOn(DatabaseConnection.prototype, "getCollection")
+      .mockImplementation(mockGetCollection);
+
+    const mockInstance = new DatabaseConnection();
+    jest.spyOn(DatabaseConnection, "getInstance").mockReturnValue(mockInstance);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("register a new user, expect success", async () => {
+    const mockCollection = {
+      findOne: jest.fn().mockResolvedValue(null),
+      insertOne: jest.fn().mockResolvedValue({ insertedId: "12345" })
+    };
+    mockGetCollection.mockReturnValue(mockCollection);
+
+    const payload = {
+      name: "name lastname",
+      email: "def@example.com",
+      password: "123456"
+    };
+
+    const res = await request(app)
+      .post("/register")
+      .send(payload)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ userId: "12345" });
+  });
 });
