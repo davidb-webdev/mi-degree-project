6426c6ec03195ac00e2e5638e0316545f840721b
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-12-23 17:28:58 +0100

    add more /register tests

--- server/__tests__/auth.test.ts
+++ server/__tests__/auth.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import app from "../app";
 import DatabaseConnection from "../database/DatabaseConnection";
+import { User } from "../models/User";
 
 jest.mock("mongodb");
 
@@ -27,17 +28,21 @@ describe("Auth endpoints", () => {
     jest.clearAllMocks();
   });
 
-  test("register a new user, expect success", async () => {
+  //
+  // ========== Register ==========
+  //
+
+  test("registering a new user, expect success", async () => {
     const mockCollection = {
       findOne: jest.fn().mockResolvedValue(null),
       insertOne: jest.fn().mockResolvedValue({ insertedId: "12345" })
     };
     mockGetCollection.mockReturnValue(mockCollection);
 
-    const payload = {
+    const payload: User = {
       name: "name lastname",
-      email: "def@example.com",
-      password: "123456"
+      email: "email@example.com",
+      password: "1234567890"
     };
 
     const res = await request(app)
@@ -48,5 +53,58 @@ describe("Auth endpoints", () => {
 
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({ userId: "12345" });
+  });
+
+  test("registering a new user, email already used, expect failure", async () => {
+    const mockCollection = {
+      findOne: jest.fn().mockResolvedValue({
+        _id: "12345",
+        name: "existing user",
+        email: "email@example.com",
+        password: "abcdefghij"
+      }),
+      insertOne: jest.fn().mockResolvedValue({ insertedId: "12345" })
+    };
+    mockGetCollection.mockReturnValue(mockCollection);
+
+    const payload: User = {
+      name: "name lastname",
+      email: "email@example.com",
+      password: "1234567890"
+    };
+
+    const res = await request(app)
+      .post("/register")
+      .send(payload)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error", "E-mail address already used");
+  });
+
+  test("registering a new user, incorrect input data, expect failure", async () => {
+    const mockCollection = {
+      findOne: jest.fn().mockResolvedValue(null),
+      insertOne: jest.fn().mockResolvedValue({ insertedId: "12345" })
+    };
+    mockGetCollection.mockReturnValue(mockCollection);
+
+    const payload = {
+      name: "name lastname",
+      email: "email@example.com"
+    };
+
+    const res = await request(app)
+      .post("/register")
+      .send(payload)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty(
+      "error",
+      'Validation error: "password" is required'
+    );
   });
 });
