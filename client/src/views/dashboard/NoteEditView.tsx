5eb5e0738ae6f28424326556576f5615c86a9522
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-12-31 00:47:34 +0100

    make NoteEditView functional

--- client/src/views/dashboard/NoteEditView.tsx
+++ client/src/views/dashboard/NoteEditView.tsx
@@ -1,33 +1,50 @@
 import { Button, Stack, TextField } from "@mui/material";
-import { Note, NoteCategory } from "../../models/Note";
 import ModalToolbar from "../../components/ModalToolbar";
-import { useNavigate, useLocation } from "react-router";
+import { useLocation } from "react-router";
 import { useTranslation } from "react-i18next";
+import { useNote } from "../../utils/useNote";
+import { useCustomParams } from "../../utils/useCustomParams";
+import { ChangeEvent, FormEvent, useState } from "react";
+import useAxios from "../../utils/useAxios";
+import { useSnackbar } from "../../utils/useSnackbar";
+import { Note, NoteCategories } from "../../models/Note";
 import SelectWithPredefinedList from "../../components/SelectWithPredefinedList";
-import { useEffect, useState } from "react";
 
 interface EditNoteViewProps {
   newNote?: boolean;
 }
 
 const EditNoteView = ({ newNote }: EditNoteViewProps) => {
-  const [note, setNote] = useState<Note>();
-  const navigate = useNavigate();
+  const [formData, setFormData] = useState(
+    new Note("", NoteCategories.BlockedEscapeRoute, "", 1, 1)
+  );
+  const apiClient = useAxios();
+  const snackbar = useSnackbar();
+  const { note, setNote } = useNote();
+  const { navigateWithParams } = useCustomParams();
   const location = useLocation();
   const { t } = useTranslation("translation", {
     keyPrefix: "dashboard.noteEdit"
   });
 
-  useEffect(() => {
-    setNote({
-      title: "Note 1",
-      category: "BlockedEscapeRoute",
-      description: "TODO"
-    });
-  }, []);
+  const handleFormChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await apiClient.patch(`/api/note/${note!._id}`, formData);
+    setNote({ ...note!, ...formData });
+    snackbar.open("success", t("success"));
+    navigateWithParams(`/dashboard/note/`);
+  };
 
   return note ? (
-    <>
+    <form onSubmit={onSubmit}>
       <ModalToolbar
         title={newNote ? t("newNote") : note.title}
         backPath={
@@ -38,41 +55,42 @@ const EditNoteView = ({ newNote }: EditNoteViewProps) => {
                 location.pathname.lastIndexOf("/")
               )}`
         }
-        actionButton={
-          <Button
-            onClick={() => {
-              console.log("Note saved");
-
-              // TODO: Send request
-              const returnedId = "1";
-
-              navigate(`/dashboard/notes/${returnedId}`);
-            }}
-          >
-            {t("save")}
-          </Button>
-        }
+        actionButton={<Button type="submit">{t("save")}</Button>}
       />
 
       <Stack sx={{ mx: 3 }} spacing={2}>
-        <TextField label={t("title")} />
+        <TextField
+          label={t("title")}
+          name="title"
+          value={formData.title}
+          onChange={handleFormChange}
+          required
+        />
 
         <SelectWithPredefinedList
           list="noteCategories"
           label={t("category")}
-          onChange={(value) =>
-            setNote({ ...note, category: value as NoteCategory })
+          name="category"
+          onChange={(event) =>
+            handleFormChange(event as ChangeEvent<HTMLInputElement>)
           }
           value={note.category}
         />
 
-        <TextField multiline label={t("description")} />
+        <TextField
+          label={t("description")}
+          name="description"
+          value={formData.description}
+          onChange={handleFormChange}
+          multiline
+          required
+        />
 
         <Button>{t("addPhotos")}</Button>
 
         <Button>{t("editLocation")}</Button>
       </Stack>
-    </>
+    </form>
   ) : (
     <>{t("notLoaded")}</>
   );
