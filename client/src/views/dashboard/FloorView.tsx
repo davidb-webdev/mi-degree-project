8a98f01cb695a4fa48122b2aa5b2f3f280319d27
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-12-31 20:22:46 +0100

    refactor project/floor view

--- client/src/views/dashboard/FloorView.tsx
+++ client/src/views/dashboard/FloorView.tsx
@@ -1,28 +1,11 @@
-import { Button } from "@mui/material";
-import { useFloors } from "../../utils/useFloors";
 import { useFloor } from "../../utils/useFloor";
 import { useCustomParams } from "../../utils/useCustomParams";
 
 const FloorView = () => {
-  const { floors } = useFloors();
   const { floor } = useFloor();
-  const { updateParams } = useCustomParams();
+  const { getParam } = useCustomParams();
 
-  return (
-    <>
-      {floors.map((floor) => (
-        <Button
-          key={floor._id}
-          onClick={() => {
-            updateParams({ f: floor._id }, ["n"]);
-          }}
-        >
-          {floor.title}
-        </Button>
-      ))}
-      {floor?.title}
-    </>
-  );
+  return !getParam("f") ? <></> : <>{floor?.title}</>;
 };
 
 export default FloorView;
--- client/src/views/dashboard/ProjectView.tsx
+++ client/src/views/dashboard/ProjectView.tsx
@@ -1,7 +1,27 @@
+import { Button } from "@mui/material";
 import FloorView from "./FloorView";
+import { useFloors } from "../../utils/useFloors";
+import { useCustomParams } from "../../utils/useCustomParams";
 
 const ProjectView = () => {
-  return <FloorView />;
+  const { floors } = useFloors();
+  const { navigateAndUpdateParams } = useCustomParams();
+
+  return (
+    <>
+      {floors.map((floor) => (
+        <Button
+          key={floor._id}
+          onClick={() => {
+            navigateAndUpdateParams("/dashboard", { f: floor._id }, ["n"]);
+          }}
+        >
+          {floor.title}
+        </Button>
+      ))}
+      <FloorView />
+    </>
+  );
 };
 
 export default ProjectView;
