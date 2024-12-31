5ebdea64eda429b3122b2cb1a68fff8501e2f05e
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-12-31 23:24:10 +0100

    add floor plan select list

--- client/src/views/dashboard/ProjectView.tsx
+++ client/src/views/dashboard/ProjectView.tsx
@@ -1,25 +1,51 @@
-import { Button } from "@mui/material";
-import FloorView from "./FloorView";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent
+} from "@mui/material";
+import FloorPlanView from "./FloorPlanView";
 import { useFloors } from "../../utils/useFloors";
 import { useCustomParams } from "../../utils/useCustomParams";
+import { useTranslation } from "react-i18next";
 
 const ProjectView = () => {
   const { floors } = useFloors();
-  const { navigateAndUpdateParams } = useCustomParams();
+  const { getParam, navigateAndUpdateParams } = useCustomParams();
+  const { t } = useTranslation("translation", {
+    keyPrefix: "dashboard.project"
+  });
+
+  const onChange = (event: SelectChangeEvent) => {
+    if (event.target.value === "editFloors") {
+      navigateAndUpdateParams("/dashboard/floors", {}, ["n"]);
+    } else {
+      navigateAndUpdateParams("/dashboard", { f: event.target.value ?? "" }, [
+        "n"
+      ]);
+    }
+  };
 
   return (
     <>
-      {floors.map((floor) => (
-        <Button
-          key={floor._id}
-          onClick={() => {
-            navigateAndUpdateParams("/dashboard", { f: floor._id }, ["n"]);
-          }}
+      <FormControl fullWidth>
+        <Select
+          name="floor"
+          labelId="floorLabel"
+          onChange={(event) => onChange(event)}
+          value={getParam("f") ?? ""}
+          inputProps={{ "aria-label": t("floor") }}
         >
-          {floor.title}
-        </Button>
-      ))}
-      <FloorView />
+          {floors.map((floor) => (
+            <MenuItem value={floor._id} key={floor._id}>
+              {floor.title}
+            </MenuItem>
+          ))}
+          <MenuItem value="editFloors">{t("editFloors")}</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FloorPlanView />
     </>
   );
 };
