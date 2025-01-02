9ece96ebaa8d9fb5e69cc65587abcc85a15895ba
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2025-01-02 09:22:42 +0100

    make docx fn functional

--- server/controllers/document.ts
+++ server/controllers/document.ts
@@ -6,17 +6,16 @@ import {
   TypedResponse
 } from "../models/Express";
 import { NextFunction } from "express";
-import { ObjectId } from "mongodb";
-import { WithId } from "../models/Mongodb";
 import { generateDocument } from "../functions/docx";
 
 export const postDocument = async (
-  req: TypedRequestParams<{ projectId: string; language: "sv" | "en" }>,
+  req: TypedRequestParams<{ projectId: string; language: string }>,
   res: TypedResponse<{ success: boolean }>,
   next: NextFunction
 ) => {
   try {
-    const documentResponse = generateDocument();
+    const { projectId, language } = req.body;
+    const documentResponse = await generateDocument(projectId, language);
     res.json({ success: true });
   } catch (error: unknown) {
     next(error);
--- server/functions/docx.ts
+++ server/functions/docx.ts
@@ -1,50 +1,135 @@
 import * as fs from "fs";
 import { Document, HeadingLevel, ImageRun, Packer, Paragraph } from "docx";
+import { ObjectId } from "mongodb";
+import DatabaseConnection from "../database/DatabaseConnection";
+import { imageSize } from "image-size";
+import en from "../language/en.json";
+import sv from "../language/sv.json";
 
-export const generateDocument = () => {
+export const generateDocument = async (
+  projectId: string,
+  language: "en" | "sv"
+) => {
+  const floors = await DatabaseConnection.getInstance().getFloorsByProjectId(
+    new ObjectId(projectId)
+  );
+
+  let translation;
+  switch (language) {
+    case "en": {
+      translation = en;
+      break;
+    }
+    case "sv": {
+      translation = sv;
+      break;
+    }
+  }
+
+  const project = await DatabaseConnection.getInstance().getProjectById(
+    new ObjectId(projectId)
+  );
+	
+  // TODO: Get user info (name)
+
+  let docFloorPlans: Paragraph[] = [];
+  let docNotes: Paragraph[] = [];
+  for (const floor of floors) {
+    const meta = imageSize("files/floorplans/fp.png"); // TODO: Get real images
+    let calculatedHeight = 0;
+    let fileType = "jpg";
+    if (
+      !meta.width ||
+      !meta.height ||
+      !meta.type ||
+      !(
+        meta.type === "jpg" ||
+        meta.type === "png" ||
+        meta.type === "gif" ||
+        meta.type === "bmp"
+      )
+    ) {
+      throw new Error("Incorrect image metadata");
+    }
+    const aspectRatio = meta.width / meta.height;
+    calculatedHeight = 400 * aspectRatio;
+    fileType = meta.type;
+
+    docFloorPlans.push(
+      new Paragraph({
+        text: floor.title,
+        heading: HeadingLevel.HEADING_2
+      }),
+      new Paragraph({
+        children: [
+          new ImageRun({
+            type: fileType as "jpg" | "png" | "gif" | "bmp",
+            // data: fs.readFileSync(floor.floorPlanPath), // TODO: Get real images
+            data: fs.readFileSync("files/floorplans/fp.png"),
+            transformation: {
+              width: 400,
+              height: calculatedHeight ?? 300
+            }
+          })
+        ]
+      })
+    );
+
+    const notes = await DatabaseConnection.getInstance().getNotesByFloorId(
+      new ObjectId(floor._id)
+    );
+
+    for (const note of notes) {
+      docNotes.push(
+        new Paragraph({
+          text: note.title,
+          heading: HeadingLevel.HEADING_2
+        }),
+        new Paragraph({
+          text: note.description
+        })
+      );
+    }
+  }
+
   const doc = new Document({
-    creator: "TODOcreator",
-    description: "TODOdesc",
-    title: "TODOtitle",
+    creator: "TODO",
+    title: project.title,
+    description: translation.title,
     sections: [
       {
         children: [
           new Paragraph({
-            text: "Besiktningsprotokoll",
+            text: translation.title,
             heading: HeadingLevel.TITLE
           }),
           new Paragraph({
-            text: "Kontrollant: TODO"
-          }),
-          new Paragraph({
-            text: "Datum: "
-          }),
-          new Paragraph({
-            text: "Anmärkningar",
+            text: project.title,
             heading: HeadingLevel.HEADING_1
           }),
           new Paragraph({
-            text: "Anteckning: Titel",
+            text: `${translation.inspector}: TODO`
+          }),
+          new Paragraph({
+            text: `${translation.date}: `
+          }),
+          new Paragraph({
+            text: translation.projectDescription,
             heading: HeadingLevel.HEADING_2
           }),
           new Paragraph({
-            text: "Anteckning: Beskrivning"
+            text: project.description
           }),
           new Paragraph({
-            children: [
-              new ImageRun({
-                type: "png",
-                data: fs.readFileSync("files/floorplans/fp.png"),
-                transformation: {
-                  width: 400,
-                  height: 400
-                }
-              })
-            ]
+            text: translation.floorPlans,
+            heading: HeadingLevel.HEADING_1
           }),
+          ...docFloorPlans,
           new Paragraph({
-            text: "Beskrivning"
-          })
+            text: translation.notes,
+            heading: HeadingLevel.HEADING_1
+          }),
+          ...docNotes
         ]
       }
     ]
