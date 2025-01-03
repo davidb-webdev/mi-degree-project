cbf754deedb714431bc80f1ff8b77b9a65a2eadb
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2025-01-03 14:11:36 +0100

    fix document download

--- server/controllers/document.ts
+++ server/controllers/document.ts
@@ -1,20 +1,17 @@
 import DatabaseConnection from "../database/DatabaseConnection";
-import {
-  TypedRequestBody,
-  TypedResponse
-} from "../models/Express";
+import { TypedRequestBody, TypedResponse } from "../models/Express";
 import { NextFunction } from "express";
 import { generateDocument } from "../functions/docx";
 
 export const postDocument = async (
   req: TypedRequestBody<{ projectId: string; language: "en" | "sv" }>,
-  res: TypedResponse<{ success: boolean }>,
+  res: TypedResponse<{ documentPath: string }>,
   next: NextFunction
 ) => {
   try {
     const { projectId, language } = req.body;
-    const documentResponse = await generateDocument(projectId, language);
-    res.json({ success: true });
+    const documentPath = await generateDocument(projectId, language);
+    res.json({ documentPath });
   } catch (error: unknown) {
     next(error);
   }
